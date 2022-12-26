c4039d80a7e1c414f3b167f3d326670c26845531
MichaelSebero <greenlightsuccess@protonmail.com>
2022-12-26 15:07:47 -0500

    e

--- CoolRune-Install.sh
+++ CoolRune-Install.sh
@@ -41,5 +41,16 @@ clear
 
 pacman-key --populate artix
 pacman-key --populate archlinux
-pacman -Sy git
-git clone https://github.com/MichaelSebero/CoolRune-Files.git
+pacman -Sy
+pacman -S --noconfirm --needed kate librewolf flatpak unzip 
+flatpak install celluloid gwe libreoffice appimagepool 
+curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-Files.tar.gz
+curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-Dotfiles.tar.gz
+cd /home/$USER/
+unzip CoolRune-Files.tar.gz -d /
+unzip CoolRune-Files.tar.gz
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key FBA220DFC880C036
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+yay -S --noconfirm --needed protontricks-git proton-ge-custom-bin xfce-superkey-git simple-mtpfs task-spooler zsh-fast-syntax-highlighting-git mystiq appimagelauncher pamac-nosnap opendoas
+
