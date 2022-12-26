966660e8aec56152280c1309beb33dc9b9413fde
MichaelSebero <greenlightsuccess@protonmail.com>
2022-12-26 12:02:01 -0500

    e
    e

--- CoolRune-Install.sh
+++ CoolRune-Install.sh
@@ -6,10 +6,10 @@
 pacman -Sy git
 git clone https://github.com/MichaelSebero/CoolRune-Files.git
 
-cd CoolRune-Files/Desktop/CoolRune-Files-Install.sh
+cd home/$USER/CoolRune-Files/Desktop/CoolRune-Files-Install.sh
 sh CoolRune-Files-Install.sh
 exit
-cd CoolRune-Files/Desktop/CoolRune-NVIDIA-Patch.sh
+cd home/$USER/CoolRune-Files/Desktop/CoolRune-NVIDIA-Patch.sh
 sh CoolRune-NVIDIA-Patch.sh
 exit
 
