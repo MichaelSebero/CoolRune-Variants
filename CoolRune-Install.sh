16f6b98e99100e40720789789a8dbf9198697a94
MichaelSebero <greenlightsuccess@protonmail.com>
2022-12-26 11:43:30 -0500

    Update CoolRune-Install.sh

--- CoolRune-Install.sh
+++ CoolRune-Install.sh
@@ -2,33 +2,14 @@
 
 # License: GNU GPLv3
 
-### OPTIONS AND VARIABLES ###
+### CoolRune Install
 pacman -Sy git
 git clone https://github.com/MichaelSebero/CoolRune-Files.git
-/home/$USER/CoolRune-Files/Desktop/CoolRune-Files-Install.sh
-/home/$USER/CoolRune-Files/Desktop/CoolRune-NVIDIA-Patch.sh
-pacman -Sy whiptail
 
-### FUNCTIONS ###
+cd CoolRune-Files/Desktop/CoolRune-Files-Install.sh
+sh CoolRune-Files-Install.sh
+exit
+cd CoolRune-Files/Desktop/CoolRune-NVIDIA-Patch.sh
+sh CoolRune-NVIDIA-Patch.sh
+exit
 
-welcomemsg() {
-	whiptail --title "CoolRune" \
-		--msgbox "Welcome to the CoolRune setup\\n\\nThis script will automatically install CoolRune on your computer.\\n\\" 10 60
-
-	whiptail --title "CoolRune" --yes-button "All ready!" \
-		--no-button "Return..." \
-		--yesno "Be sure the computer you are using has current pacman updates and refreshed Arch keyrings.\\n\\nIf it does not, the installation of some programs might fail." 8 70
-}
-
-InstallingCoolRune() {
-	whiptail --infobox "Installing CoolRune" 7 40
-		pacman --noconfirm -Sy artix-keyring >/dev/null 2>&1
-		;;
-
-		pacman	artix-archlinux-support >/dev/null 2>&1
-
-		pacman-key --populate artix >/dev/null 2>&1
-		pacman-key --populate archlinux >/dev/null 2>&1
-		;;
-	esac
-}
