a3f6d52ed79babb779a264d21b4ee0d2d9a4269d
MichaelSebero <greenlightsuccess@protonmail.com>
2022-12-26 13:00:17 -0500

    Update CoolRune-Install.sh

--- CoolRune-Install.sh
+++ CoolRune-Install.sh
@@ -4,6 +4,43 @@
 
 ### OPTIONS AND VARIABLES ###
 
+clear
+
+echo '
+[universe]
+Server = https://universe.artixlinux.org/$arch
+Server = https://mirror1.artixlinux.org/universe/$arch
+Server = https://mirror.pascalpuffke.de/artix-universe/$arch
+Server = https://artixlinux.qontinuum.space/artixlinux/universe/os/$arch
+Server = https://mirror1.cl.netactuate.com/artix/universe/$arch
+Server = https://ftp.crifo.org/artix-universe/' >> /etc/pacman.conf
+
+pacman -Sy
+pacman -S artix-archlinux-support
+
+echo '
+# Arch Linux repos
+#[testing]
+#Include = /etc/pacman.d/mirrorlist
+#[core]
+#Include = /etc/pacman.d/mirrorlist-arch
+[extra]
+Include = /etc/pacman.d/mirrorlist-arch
+#[community-testing]
+#Include = /etc/pacman.d/mirrorlist
+[community]
+Include = /etc/pacman.d/mirrorlist-arch
+#[multilib-testing]
+#Include = /etc/pacman.d/mirrorlist
+[multilib]
+Include = /etc/pacman.d/mirrorlist-arch
+' >> /etc/pacman.conf
+
+clear
+
+pacman-key --populate archlinux
+pacman -Sy
+
 dotfilesrepo="https://github.com/MichaelSebero/CoolRune-Files.git"
 progsfile="https://raw.githubusercontent.com/MichaelSebero/CoolRune-NVIDIA/master/progs.csv"
 aurhelper="yay"
