f95cdefff5d43dbf5870d759ebd5050cbd1e1d21
MichaelSebero <greenlightsuccess@protonmail.com>
2022-12-28 15:31:13 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@
 ### CoolRune Repositories ###
 pacman-key --init && pacman -Sy --noconfirm --needed p7zip && mkdir /home/CoolRune-Files && cd /home/CoolRune-Files && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-Pacman-1.7z && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-Pacman-2.7z && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-Files.7z && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-NVIDIA-Patch.7z && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-Dotfiles.7z &&
 # CoolRune Last Commands
-7z e CoolRune-Files.7z -f -aoa / -y && 7z e CoolRune-NVIDIA-Patch.7z -f -aoa / -y && 7z e CoolRune-Dotfiles.7z -f -aoa /home/$USER -y && update-grub && s6-db-reload && reboot
+7z e CoolRune-Files.7z --overwrite -aoa / -y && 7z e CoolRune-NVIDIA-Patch.7z --overwrite -aoa / -y && 7z e CoolRune-Dotfiles.7z --overwrite -aoa /home/$USER -y && update-grub && s6-db-reload && reboot
