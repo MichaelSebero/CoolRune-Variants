65ce9c82350857744a11443a48593ba2a561a627
MichaelSebero <greenlightsuccess@protonmail.com>
2022-12-28 05:50:20 -0500

    .

--- CoolRune-Install.sh
+++ CoolRune-Install.sh
@@ -4,5 +4,11 @@
 
 ### OPTIONS AND VARIABLES ###
 
-pacman-key --init && pacman -Sy --noconfirm --needed unzip && ln -s /home/CoolRune-Files ~/CoolRune-Files && cd ~/CoolRune-Files && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-Pacman-1.zip && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-Files.zip && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-NVIDIA-Patch.zip && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-Dotfiles.zip && cd /home/CoolRune-Files/ && unzip CoolRune-Pacman-1.zip -d /etc && pacman -Sy --noconfirm --needed archlinux-keyring artix-keyring artix-archlinux-support && pacman-key --populate archlinux artix && pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com && pacman-key --lsign-key FBA220DFC880C036 && pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' && unzip CoolRune-Pacman-2.zip -d /etc && pacman -Sy
+pacman-key --init && pacman -Sy --noconfirm --needed unzip && ln -s /home/CoolRune-Files ~/CoolRune-Files &&
+
+echo '
+alias CoolRune-Files="cd /home/CoolRune-Files"
+' >> /home/$USER/.bashrc
+
+&& cd ~/CoolRune-Files && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-Pacman-1.zip && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-Files.zip && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-NVIDIA-Patch.zip && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-Dotfiles.zip && cd /home/CoolRune-Files/ && unzip CoolRune-Pacman-1.zip -d /etc && pacman -Sy --noconfirm --needed archlinux-keyring artix-keyring artix-archlinux-support && pacman-key --populate archlinux artix && pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com && pacman-key --lsign-key FBA220DFC880C036 && pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' && unzip CoolRune-Pacman-2.zip -d /etc && pacman -Sy
 
