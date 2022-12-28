e8f96b1f2770475728ccbbe8f6c5bc0e6fe0da2a
MichaelSebero <greenlightsuccess@protonmail.com>
2022-12-28 10:17:44 -0500

    Update CoolRune-Install.sh

--- CoolRune-Install.sh
+++ CoolRune-Install.sh
@@ -9,13 +9,13 @@ pacman-key --init && pacman -Sy --noconfirm --needed p7zip && mkdir /home/CoolRu
 pacman -S --noconfirm --needed lib32-artix-archlinux-support flatpak kate librewolf python-pip tmux tealdeer jdownloader2 vulkan-icd-loader lib32-vulkan-icd-loader ksysguard liferea gnome-screenshot gnome-keyring gnome-calculator font-manager gwenview gimp gamemode lib32-gamemode filelight fail2ban fail2ban-s6 evince dnscrypt-proxy dnscrypt-proxy-s6 apparmor apparmor-s6 bleachbit blueman unrar bluez-s6 konsole catfish clamav clamav-s6 ark devilspie gufw mugshot macchanger networkmanager networkmanager-s6 nm-connection-editor wine wine-mono winetricks ufw-s6 pcmanfm pavucontrol qbittorrent redshift pulseaudio-alsa psensor ntfs-3g steam lynis sneedacity element-desktop pulsemixer moreutils rkhunter yay nvidia-dkms lib32-nvidia-utils nvidia-utils nvidia-settings nvidia-utils-s6 &&
 
 # FLATPAK FILES
-flatpak install io.github.celluloid_player.Celluloid com.leinardi.gwe org.libreoffice.LibreOffice io.github.prateekmedia.appimagepool &&
+flatpak install -y io.github.celluloid_player.Celluloid com.leinardi.gwe org.libreoffice.LibreOffice io.github.prateekmedia.appimagepool &&
 
 # PYTHON FILES
 pip install psutil python-dateutil protonvpn-cli &&
 
 # AUR REPO FILES
-yay -S --noconfirm --needed protontricks-git proton-ge-custom-bin xfce-superkey-git simple-mtpfs task-spooler zsh-fast-syntax-highlighting-git mystiq appimagelauncher opendoas linux-tkg-pds linux-tkg-pds-headers pamac-nosnap &&
+yay -S --noconfirm --needed protontricks-git proton-ge-custom-bin xfce-superkey-git simple-mtpfs task-spooler zsh-fast-syntax-highlighting-git mystiq appimagelauncher opendoas pamac-nosnap && yay -S linux-tkg-pds linux-tkg-pds-headers
 
 # CoolRune Last Commands
 7z e CoolRune-Files.7z -o/ && 7z e CoolRune-NVIDIA-Patch.7z -o/ && 7z e CoolRune-Dotfiles.7z -o/home/$USER &&
